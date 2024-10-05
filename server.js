3616b63cc569739caccae1258f8e29d45b261056
Utkarsh1228 <110552854+Utkarsh1228@users.noreply.github.com>
2024-10-05 14:37:34 +0530

    added the frontend-url

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
+import cors from "cors";
 
 const app = express();
 
@@ -23,6 +24,13 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+
+// Allow CORS from your frontend URL
+app.use(cors({
+  origin: "https://netfliixby-utkarsh.netlify.app/", // Change this to your actual frontend URL
+  credentials: true // If you are using cookies or other credentials
+}));
 
 if (ENV_VARS.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
